4a4fff7e435e471042cf0a071a9f255cc8560838
Drew Rollins <drewrollins14@gmail.com>
2018-05-02 17:11:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = new Object({})
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({recipes}, {prop1: 1, prop2: 2})
+  Object.assign({object}, {prop1: 1, prop2: 2})
   return recipes
 }
 
